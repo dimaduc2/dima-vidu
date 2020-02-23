8a33b4bc8ff6a1a04c146b3dc5703721d693e38c
Dima Duc <dimaduc@gmail.com>
2020-02-23 09:23:16 +0700

    Updates

---
+++ precache-manifest.b538b4fa4f762e85568b14cbaa89af7a.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd55dd6fc9f91cdf1d1d0eaf2aeb2619",
+    "url": "/dima-vidu/index.html"
+  },
+  {
+    "revision": "8bb9c7ac56042382d25a",
+    "url": "/dima-vidu/static/css/2.7220993a.chunk.css"
+  },
+  {
+    "revision": "5dc5949c549fde6e50d6",
+    "url": "/dima-vidu/static/css/main.66e329c5.chunk.css"
+  },
+  {
+    "revision": "8bb9c7ac56042382d25a",
+    "url": "/dima-vidu/static/js/2.034a9153.chunk.js"
+  },
+  {
+    "revision": "5dc5949c549fde6e50d6",
+    "url": "/dima-vidu/static/js/main.1fa3dac4.chunk.js"
+  },
+  {
+    "revision": "00ce3cbafe5c5f8f4d8e",
+    "url": "/dima-vidu/static/js/runtime-main.07333c2f.js"
+  },
+  {
+    "revision": "3d7c74ac555da8f19370d2cf1aaed0a7",
+    "url": "/dima-vidu/static/media/Anakin_Skywalker.3d7c74ac.jpg"
+  },
+  {
+    "revision": "651e34a2a45a7844e8d5458b5fd99fb0",
+    "url": "/dima-vidu/static/media/Anakin_Vader.651e34a2.jpg"
+  },
+  {
+    "revision": "5cc022543d3a0972a347062fb5fb6a65",
+    "url": "/dima-vidu/static/media/Anakin_Vader_Skywalker.5cc02254.jpg"
+  },
+  {
+    "revision": "b0b31d22606d76320b081117b64cd151",
+    "url": "/dima-vidu/static/media/Cherish Ball.b0b31d22.jpg"
+  },
+  {
+    "revision": "401012795dcb6e03d186e59d201a667b",
+    "url": "/dima-vidu/static/media/Eevee mega evolution.40101279.jpg"
+  },
+  {
+    "revision": "ad9fe204d9e2c5d9a3984f6fcaadf619",
+    "url": "/dima-vidu/static/media/Great Ball.ad9fe204.jpg"
+  },
+  {
+    "revision": "5c8f3d183fab336077cb2836f09eb6a7",
+    "url": "/dima-vidu/static/media/KYLO REN VS. DARTH VADER IN REAL LIFE.5c8f3d18.jpg"
+  },
+  {
+    "revision": "a7450025846f2b1aa85bdae130b7fb42",
+    "url": "/dima-vidu/static/media/Obi-Wan_Kenobi_Jar_Kai.a7450025.jpg"
+  },
+  {
+    "revision": "5f8298d5ba69f3ee04c2041b9e30de5c",
+    "url": "/dima-vidu/static/media/Qui_Gon_Jinn.5f8298d5.jpg"
+  },
+  {
+    "revision": "f00ac0783839f481c244d8c41b27ce3e",
+    "url": "/dima-vidu/static/media/Siri_Tachi.f00ac078.jpg"
+  },
+  {
+    "revision": "a9446d3fcdc3e4b27512c2ac67c23721",
+    "url": "/dima-vidu/static/media/Sith_Padme.a9446d3f.jpg"
+  },
+  {
+    "revision": "0430331b2e63ce822c82d4ea0bd91ca3",
+    "url": "/dima-vidu/static/media/Tahl.0430331b.jpg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/dima-vidu/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/dima-vidu/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/dima-vidu/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/dima-vidu/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/dima-vidu/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/dima-vidu/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/dima-vidu/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/dima-vidu/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/dima-vidu/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/dima-vidu/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/dima-vidu/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/dima-vidu/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/dima-vidu/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/dima-vidu/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/dima-vidu/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/dima-vidu/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/dima-vidu/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
